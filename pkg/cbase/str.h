7f104bb11250557f6559de1fe75de469e8506376
Ryan Sepassi <rsepassi@gmail.com>
2024-09-02 12:24:57 -0700

    cbase: fix str list interop

--- pkg/cbase/str.h
+++ pkg/cbase/str.h
@@ -1,6 +1,8 @@
 #ifndef BASE_STR_H_
 #define BASE_STR_H_
 
+#include "base/list.h"
+
 typedef struct {
   size_t len;
   const char* bytes;
@@ -10,9 +12,8 @@ typedef struct {
 #define str_init(s, l) ((str_t){.bytes = s, .len = l})
 #define cstr(x) ((str_t){.bytes = x, .len = strlen(x)})
 
-typedef struct list_s list_t;
 inline str_t str_from_list(list_t l) {
-  return (str_t){.bytes = l.base, .len = l.len};
+  return (str_t){.bytes = (char*)l.base, .len = l.len};
 }
 
 #define str_append(list, s, len) do {\

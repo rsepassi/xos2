e052573f14d68f088cfc3e7489304b826ca12cd1
Ryan Sepassi <rsepassi@gmail.com>
2024-09-16 13:32:30 -0700

    tidy: update traversal in demo

--- pkg/tidy/demo.c
+++ pkg/tidy/demo.c
@@ -7,6 +7,37 @@
 
 #define MAX_DEPTH 4
 
+typedef struct {
+  TidyNode* stack;  // each entry is the child pointer for that level
+  size_t stack_len;
+  int top;  // index of first NULL node
+} tidy_stack_t;
+
+TidyNode traverse(TidyDoc tdoc, tidy_stack_t* stack) {
+  // While we're not done with the root node
+  if (stack->top <= 0) return 0;
+
+  // Continue processing the children for the node at the top of the stack
+  TidyNode cur = stack->stack[stack->top - 1];
+
+  // If we've reached the end of the nodes at this level, pop the stack until
+  // we find something not null.
+  while (stack->top > 0 && !cur) cur = stack->stack[--stack->top - 1];
+
+  // If cursor is still null, we're all done.
+  if (!cur) return 0;
+
+  // Otherwise, we've found our current node. Advance for this level.
+  stack->stack[stack->top - 1] = tidyGetNext(cur);
+
+  // If this is the start of a node, push to the stack before returning
+  // the current node.
+  if (tidyNodeGetType(cur) == TidyNode_Start)
+    stack->stack[stack->top++] = tidyGetChild(cur);
+
+  return cur;
+}
+
 int main(int argc, char** argv) {
   char* input = "<foobar big=z><foo/><contents attr=7>Hi</contents><meta><a>Bye</a><b/></meta></foobar>";
   TidyDoc tdoc = tidyCreate();
@@ -21,47 +52,28 @@ int main(int argc, char** argv) {
   tidyBufInit(&buf);
 
   // Iterative depth-first traversal of node n
-  TidyNode stack[MAX_DEPTH];  // each entry is the child pointer for that level
-  int stack_top = 0;
-  stack[stack_top] = tidyGetChild(n);
+  tidy_stack_t stack = {0};
+  TidyNode stack_buf[MAX_DEPTH];
+  stack.stack = stack_buf;
+  stack.stack_len = MAX_DEPTH;
+  stack.stack[stack.top++] = tidyGetChild(n);
 
-  // While we're not done with the root node
-  while (stack_top >= 0) {
-stack_start:;
-    // Continue processing the children for the node at the top of the stack
-    TidyNode cur = stack[stack_top];
-    for (; cur; cur = tidyGetNext(cur)) {
-      TidyNodeType type = tidyNodeGetType(cur);
-      switch (type) {
-        case TidyNode_Text:
-          tidyNodeGetText(tdoc, cur, &buf);
-          LOG(": %.*s", buf.size - 1, buf.bp);
-          tidyBufClear(&buf);
-          break;
-        case TidyNode_Start:
-        case TidyNode_StartEnd:
-          LOG("< %s", tidyNodeGetName(cur));
-          for (TidyAttr attr = tidyAttrFirst(n); attr; attr = tidyAttrNext(attr)) {
-            LOG("     %s=%s", tidyAttrName(attr), tidyAttrValue(attr));
-          }
-          if (type == TidyNode_Start) {
-            // We have a new node to descend into
-            // Advance the pointer to the next child for the current level
-            stack[stack_top] = tidyGetNext(cur);
-            // Push the new node onto the top of the stack
-            stack[++stack_top] = tidyGetChild(cur);
-            goto stack_start;
-          } else {
-            LOG(">");
-          }
-          break;
-        default:
-          LOG("node skip %s %d", tidyNodeGetName(cur), type);
-      }
+  TidyNode cur;
+  while ((cur = traverse(tdoc, &stack))) {
+    switch (tidyNodeGetType(cur)) {
+      case TidyNode_Text:
+        tidyNodeGetText(tdoc, cur, &buf);
+        LOG(": %.*s", buf.size - 1, buf.bp);
+        tidyBufClear(&buf);
+        break;
+      case TidyNode_Start:
+      case TidyNode_StartEnd:
+        LOG("node %s", tidyNodeGetName(cur));
+        for (TidyAttr attr = tidyAttrFirst(cur); attr; attr = tidyAttrNext(attr)) {
+          LOG("     %s=%s", tidyAttrName(attr), tidyAttrValue(attr));
+        }
+        break;
     }
-
-    // We've made it to the end of the children at this level. Pop the stack.
-    if (--stack_top >= 0) LOG(">");
   }
 
   tidyBufFree(&buf);

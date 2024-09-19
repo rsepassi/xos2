e98c944f192ae4de6a2eb1bd0c09174f1d38a5c5
Ryan Sepassi <rsepassi@gmail.com>
2024-09-19 13:03:10 -0700

    fenster removed

--- pkg/fenster/include/fenster/fenster_mac.h
+++
@@ -1,7 +0,0 @@
-#include <CoreGraphics/CoreGraphics.h>
-#include <objc/NSObjCRuntime.h>
-#include <objc/objc-runtime.h>
-
-typedef struct {
-  id wnd;
-} fenster_platform;
--- pkg/fenster/include/fenster/fenster_win32.h
+++
@@ -1,6 +0,0 @@
-#define WIN32_LEAN_AND_MEAN
-#include <windows.h>
-
-typedef struct {
-  HWND hwnd;
-} fenster_platform;
